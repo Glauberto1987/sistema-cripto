e6c7e572cd90dfbb7aa300598a00e065b28a040e
glauberto1987 <glauberto1987@gmail.com>
2026-08-16 21:23:26 -0300

    Corrigindo import os fora da string

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 """
 Django settings for core project.
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +129,7 @@ MAILERS = {
     },
 }
 LOGIN_URL = '/admin/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
