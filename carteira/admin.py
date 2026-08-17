caa5b60e5bc254829bbba9169c5c734b381051ea
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 08:53:23 -0300

    Forcando remocao do indice negativo da views

--- carteira/admin.py
+++ carteira/admin.py
@@ -3,3 +3,9 @@ from .models import Moeda, Transacao
 
 admin.site.register(Moeda)
 admin.site.register(Transacao)
+from .models import HistoricoPatrimonio
+
+@admin.register(HistoricoPatrimonio)
+class HistoricoPatrimonioAdmin(admin.ModelAdmin):
+    list_display = ('data', 'valor_total')
+    ordering = ('-data',)
