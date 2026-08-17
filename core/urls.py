8ac1ed8c32d89843bc39f3170002a0c8b0062fb2
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 13:25:50 -0300

    Limpando views e ajustando rotas finais

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from carteira import views  # Garanta que o views está importado!
+from carteira import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('atualizar/', views.atualizar_precos, name='atualizar_precos'),
     path('moeda/<int:id>/', views.detalhe_moeda, name='detalhe_moeda'),
-    
-    # Nossa rota secreta tem que estar aqui!
-    path('abrir-cofre/', views.super_criador), 
 ]
