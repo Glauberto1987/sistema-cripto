c5a1e2154222965bf04a5c41d376fd95f2c051f8
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 13:18:53 -0300

    Adicionando a rota secreta no urls.py correto

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from carteira import views
+from carteira import views  # Garanta que o views está importado!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, name='dashboard'), 
+    path('', views.dashboard, name='dashboard'),
     path('atualizar/', views.atualizar_precos, name='atualizar_precos'),
-    path('moeda/<int:id>/', views.detalhe_moeda, name='detalhe_moeda'), # <-- Nossa nova rota de detalhes!
+    path('moeda/<int:id>/', views.detalhe_moeda, name='detalhe_moeda'),
+    
+    # Nossa rota secreta tem que estar aqui!
+    path('abrir-cofre/', views.super_criador), 
 ]
