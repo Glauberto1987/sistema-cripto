22aec07ad5b17fcb76acb3678116751aa7721927
glauberto1987 <glauberto1987@gmail.com>
2026-08-16 22:00:45 -0300

    Adicionando imagem realista como logo e fundo do login admin

--- core/settings.py
+++ core/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'carteira', # <-- Adicione esta linha aqui!
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'carteira', # <-- Adicione esta linha aqui!
+    
 ]
 
 MIDDLEWARE = [
