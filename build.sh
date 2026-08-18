a35f315ff9ca7e292dd343bbad1287d858b0a3ce
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 22:09:11 -0300

    Corrigindo o build.sh para instalar os pacotes

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='glauberto').exists() or User.objects.create_superuser('glauberto', 'a@a.com', 'admin123')"
