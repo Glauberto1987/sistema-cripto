6c1566a7e17b3e0b85669714cdce3961eb761c9f
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 22:23:55 -0300

    Automatizando a criacao do superusuario

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Codigo para burlar a falta de Shell e criar o SuperUser silenciosamente
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='glauberto').exists():
+    User.objects.create_superuser('glauberto', 'admin@email.com', 'admin123')
+EOF
