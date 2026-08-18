07d87874ccad00c973169d27675c23a6f55d460b
glauberto1987 <glauberto1987@gmail.com>
2026-08-17 21:45:40 -0300

    Liberando o dominio do Render no CSRF

--- core/settings.py
+++ core/settings.py
@@ -139,3 +139,4 @@ LOGOUT_REDIRECT_URL = '/admin/login/'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CSRF_TRUSTED_ORIGINS = ['https://sistema-cripto.onrender.com']
